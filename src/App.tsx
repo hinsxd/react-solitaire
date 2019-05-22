ccc86d4a98b3888a4597d65b09c55efc55e83cff
hinsxd <hins.xd@hotmail.com>
2019-05-23 04:00:50 +0800

    Can move card (basic)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Paper } from '@material-ui/core';
 import './App.css';
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const App: React.FC = () => {
   const classes = useStyles();
-
+  const [{ from, to }, setMove] = useState({ from: 1, to: 2 });
   const {
     state: { tableau, foundation, stock, waste },
     dispatch,
@@ -32,19 +32,46 @@ const App: React.FC = () => {
         alignItems="flex-start"
         spacing={3}
       >
-        {tableau.map(column => (
-          <Grid item xs container direction="column">
-            {column.map(card => (
-              <Grid item>
+        {tableau.map((column, i) => (
+          <Grid
+            item
+            xs
+            container
+            direction="column-reverse"
+            spacing={3}
+            key={`column${i}`}
+          >
+            {column.map((card, j) => (
+              <Grid item key={`${card.suitName}${card.rankName}`}>
                 <Paper className={classes.cardPaper}>
                   {card.suitName}
                   {card.rankName}
+                  {card.flipped && 'flipped'}
                 </Paper>
               </Grid>
             ))}
           </Grid>
         ))}
       </Grid>
+      <div>
+        From:{' '}
+        <input
+          value={from}
+          type="number"
+          onChange={({ currentTarget: { value = 1 } }) =>
+            setMove(ori => ({ ...ori, from: +value }))
+          }
+        />
+        To:{' '}
+        <input
+          value={to}
+          type="number"
+          onChange={({ currentTarget: { value = 1 } }) =>
+            setMove(ori => ({ ...ori, to: +value }))
+          }
+        />
+        <button onClick={() => move({ from, to })}>Move</button>
+      </div>
     </Container>
   );
 };
